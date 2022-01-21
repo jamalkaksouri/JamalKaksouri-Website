abbf9160865358f380fc9cdf250c9fd6b6b85512
jamalkaksouri <jamal.kaksouri@gmail.com>
2022-01-22 01:33:43 +0330

    homepage section done :rocket:

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  Link,
+  useColorModeValue,
+  Button
+} from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} textAlign="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        textAlign="center"
+      >
         Hello, I&apos;m a software engineer based on Iran.
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -13,7 +31,49 @@ const Page = () => {
           </Heading>
           <p>Betria Design (Development of smart solutions)</p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/me.jpg"
+            alt="Profile Image"
+          />
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          I&apos;m Jamal Kaksouri Seven-year of software development with
+          methods of high-performance software. I focus on SOLID principles and
+          object-oriented programming. I&apos;m well-practiced and well-versed
+          in many facets of software development: - Implementation - Design
+          Patterns - Architecture - Testing - Distributed Systems - Domain
+          Driven Design - Product Development I make software. I keep getting
+          better. program to an interface and not an implementation. See my{' '}
+          <NextLink href="/works/projects">
+            <Link>projects</Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box textAlign="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
     </Container>
   )
 }
