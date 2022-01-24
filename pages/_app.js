c9849c695d9f3389c21f4f1ac253c0feea0b6439
jamalkaksouri <jamal.kaksouri@gmail.com>
2022-01-25 01:36:17 +0330

    support detect screen orientation for small screen devices

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
-import Layout from '../components/layouts/main'
-import Fonts from '../components/fonts'
-import { AnimatePresence } from 'framer-motion'
-import Chakra from '../components/chakra'
+import Layout from "../components/layouts/main";
+import Fonts from "../components/fonts";
+import { AnimatePresence } from "framer-motion";
+import Chakra from "../components/chakra";
+import { orientedImg } from "/public/orientation-img.svg";
+import { Image } from "@chakra-ui/react";
+import { useMobileOrientation } from "react-device-detect";
+import dynamic from "next/dynamic";
 
-function Website({ Component, pageProps, router }) {
-  // chack git fata error already here
+// const DynamicComponentWithNoSSR = dynamic(() => import(Website), {
+//   ssr: false,
+// });
+
+const Website = ({ Component, pageProps, router }) => {
+  const { isLandscape } = useMobileOrientation();
   return (
+    // <DynamicComponentWithNoSSR>
     <Chakra cookies={pageProps.cookies}>
+      {isLandscape && alert("102030")}
+      {/* just for test git is support commit fetch */}
+      {/* just for test git is support commit fetch */}
+      {/* just for test git is support commit fetch */}
       <Fonts />
       <Layout router={router}>
         <AnimatePresence exitBeforeEnter initial={true}>
@@ -14,7 +27,10 @@ function Website({ Component, pageProps, router }) {
         </AnimatePresence>
       </Layout>
     </Chakra>
-  )
-}
+    // </DynamicComponentWithNoSSR>
+  );
+};
 
-export default Website
+export default dynamic(() => Promise.resolve(Website), {
+  ssr: false,
+});
