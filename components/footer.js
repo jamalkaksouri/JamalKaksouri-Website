59a7b97c5419552ec983dd2300c3a97672daf797
Jamal Kaksouri <jamal.kaksouri@gmail.com>
2022-01-25 12:38:35 +0330

    Display user information including IP address and device details

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,27 @@
 import { Box } from '@chakra-ui/react'
+import * as rdd from 'react-device-detect'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Footer = () => {
+  const getUserIpAddress = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/')
+    setIP(res.data.IPv4)
+  }
+
+  useEffect(() => {
+    getUserIpAddress()
+  }, [])
+
+  const [ip, setIP] = useState('')
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
       &copy; {new Date().getFullYear()} Jamal Kaksouri. All Rights Reserved.
+      <p>
+        Your device: {rdd.isMobileOnly && rdd.mobileVendor}-{rdd.osName}(
+        {rdd.osVersion})
+      </p>
+      <p>Your ip: {ip}</p>
     </Box>
   )
 }
